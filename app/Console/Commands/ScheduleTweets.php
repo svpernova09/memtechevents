892be2d0ee3a4a4e182b97a0e406610b87eccdb3
Joe Ferguson <joe@joeferguson.me>
2015-10-13 07:41:51 -0500

    Add tweet scheduling for events in the next 24 hours

--- app/Console/Commands/ScheduleTweets.php
+++ app/Console/Commands/ScheduleTweets.php
@@ -75,6 +75,33 @@ class ScheduleTweets extends Command
                 ]);
             }
         }
+
+        $eventsIn24hours = $this->event->getEventsWithin24Hours();
+
+        foreach ($eventsIn24hours as $event) {
+            $time = $this->getEventTime($event->time);
+            $this->info('Event: ' . $event['name'] . ' at ' . $time->format('jS \o\f M \a\t h:ia'));
+
+            $content = $event['name'] . ' at ' . $time->format('jS \o\f M \a\t h:ia') . ' #memtech ' . $event['event_url'];
+            $tweet = $this->reformatStatus($content, $event, $time->format('jS \o\f M \a\t h:ia'));
+            $tweet_at = $time->subHours(10)->setTimezone('America/Chicago');
+
+            // check if we already have a pending tweet
+
+            $existing = Tweet::where('event_id', $event['event_id'])
+                ->where('days_before', 0)
+                ->first();
+
+            if (is_null($existing)) {
+                $this->tweet->create([
+                    'content' => $tweet,
+                    'tweet_at' => $tweet_at,
+                    'sent' => false,
+                    'event_id' => $event['event_id'],
+                    'days_before' => 0,
+                ]);
+            }
+        }
     }
 
     public function reformatStatus($content, $event, $time_string)
